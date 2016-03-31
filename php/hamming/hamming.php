28e6ee4bc5df3135d7e398c448fc59dbfd6c0947
Jennifer Wu <jwu@Jennifers-MacBook-Pro.local>
2016-03-31 17:44:16 -0500

    php exercise - hamming: initial commit

---
+++ php/hamming/hamming.php
@@ -0,0 +1,17 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function distance($a, $b)
+{
+  if ($a === $b) {
+    return 0;
+  };
+
+  
+}
+
+?>
