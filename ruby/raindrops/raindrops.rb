f1865c1cbce4bb56ca2162e9bf1c2c3819aaca5e
Jennifer Wu <jw1314@gmail.com>
2016-04-02 20:28:56 -0500

    ruby exercise - raindrops: complete

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,23 @@
+class Raindrops
+  VERSION = 1
+
+  def self.convert(number)
+    if number % 105 == 0
+      'PlingPlangPlong'
+    elsif number % 35 == 0
+      'PlangPlong'
+    elsif number % 21 == 0
+      'PlingPlong'
+    elsif number % 15 == 0
+      'PlingPlang'
+    elsif number % 7 == 0
+      'Plong'
+    elsif number % 5 == 0
+      'Plang'
+    elsif number % 3 == 0
+      'Pling'
+    else
+      "#{number}"
+    end    
+  end
+end
